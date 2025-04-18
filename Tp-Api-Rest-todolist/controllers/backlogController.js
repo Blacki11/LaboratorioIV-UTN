d387b99431b14add7770f084a5c316ef78a19338
Blacki11 <jeremiasescuela@gmail.com>
2025-04-18 00:23:27 -0300

    estructure controller-tpApiRest

---
+++ Tp-Api-Rest-todolist/controllers/backlogController.js
@@ -0,0 +1,49 @@
+class backlogController{
+    constructor(){
+
+    }
+    async create(req, res){
+        try {
+            const { title, description } = req.body;
+            const task = await Backlog.create({ title, description });
+            res.status(201).json(task);
+        }
+        catch (error) {
+            res.status(500).json({ message: 'Error creating task', error });
+        }
+    }
+    async getAll(req, res){
+        try {
+
+        }
+        catch (err) {
+
+        }
+    }
+    async getOne(req, res){
+        try {
+
+        }
+        catch (err) {
+
+        }
+    }
+    async update(req, res){
+        try {
+
+        }
+        catch (err) {
+
+        }
+    }
+    async delete(req, res){
+        try {
+
+        }
+        catch (err) {
+
+        }
+    }
+}
+
+export default new backlogController();
---
+++ Tp-Api-Rest-todolist/controllers/taskController.js
@@ -0,0 +1,49 @@
+class taskController{
+    constructor(){
+
+    }
+    async create(req, res){
+        try {
+            const { title, description } = req.body;
+            const task = await Task.create({ title, description });
+            res.status(201).json(task);
+        }
+        catch (error) {
+            res.status(500).json({ message: 'Error creating task', error });
+        }
+    }
+    async getAll(req, res){
+        try {
+
+        }
+        catch (err) {
+
+        }
+    }
+    async getOne(req, res){
+        try {
+
+        }
+        catch (err) {
+
+        }
+    }
+    async update(req, res){
+        try {
+
+        }
+        catch (err) {
+
+        }
+    }
+    async delete(req, res){
+        try {
+
+        }
+        catch (err) {
+
+        }
+    }
+}
+
+export default new taskController();
